b36c494a6bde5f24f8a3da785a07a9e63b2be050
singhEkamjots <161613191+singhEkamjots@users.noreply.github.com>
2024-04-30 22:59:54 -0400

    On branch main 	modified:   contact.php 	modified:   index.html

--- contact.php
+++ contact.php
@@ -6,24 +6,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastName = $_POST["lastName"];
     $reason = $_POST["reason"];
 
-    
     $to = "ekamjot446@gmail.com"; 
-
-   
     $subject = "Contact Form Submission";
 
-    // Build email message
     $message = "First Name: " . $firstName . "\n";
     $message .= "Last Name: " . $lastName . "\n";
     $message .= "Reason for Contact: " . $reason . "\n";
 
-
     $headers = "From: " . $firstName . " " . $lastName . " <" . $to . ">" . "\r\n";
     $headers .= "Reply-To: " . $to . "\r\n";
 
-
     if (mail($to, $subject, $message, $headers)) {
-    
         echo "<p class='submission-message'>Thank you for your submission!</p>";
     } else {
         echo "<p class='error-message'>Sorry, there was an error sending your message. Please try again later.</p>";
